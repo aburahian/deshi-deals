e1475615c399e20660c88b6aaebd99c61572df35
Abu Rahian <aburaihan363@gmail.com>
2025-08-27 11:18:49 +0600

    finish

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-      const cartBtn=document.getElementsByClassName("add-cart-btn")
-      for(let btn of cartBtn){
-        btn.addEventListener("click",function(){
-         const title= btn.parentNode.children[1].children[1].innerHTML
-          const price=Number(btn.parentNode.children[1].children[2].children[0].innerHTML)
-          const cartImg=btn.parentNode.children[0].children[0].src
+document.getElementById("item-containers").addEventListener("click",function(e){
+     if(e.target.className.includes("add-cart-btn")){
+        const title= e.target.parentNode.children[1].children[1].innerHTML
+          const price=Number(e.target.parentNode.children[1].children[2].children[0].innerHTML)
+          const cartImg=e.target.parentNode.children[0].children[0].src
           
           
           const newDiv=document.createElement("div")
@@ -28,9 +26,43 @@
           
             document.getElementById("total-price").innerText=total
              document.getElementById("total-purchase").innerText=total
-        })
+     }
+  
+})
+
+
+      // const cartBtn=document.getElementsByClassName("add-cart-btn")
+      // for(let btn of cartBtn){
+      //   btn.addEventListener("click",function(){
+      //    const title= btn.parentNode.children[1].children[1].innerHTML
+      //     const price=Number(btn.parentNode.children[1].children[2].children[0].innerHTML)
+      //     const cartImg=btn.parentNode.children[0].children[0].src
+          
+          
+      //     const newDiv=document.createElement("div")
+      //     newDiv.innerHTML=`
+      //           <div class="bg-white p-5 shadow-sm">
+      //           <div class="flex justify-between items-center p-3 bg-[#F8F8F8] rounded-2xl">
+      //               <div class="">
+      //                 <img src="${cartImg}" class="w-[50%]" alt="">
+      //               </div>
+      //               <div ">
+      //                 <p>${title}</p>
+      //                 <p>${price} tk</p>
+      //               </div>
+      //           </div>
+      //       </div>
+      //     `
+      //     document.getElementById("sold-container").appendChild(newDiv)
+      //     const totalPrice=Number(document.getElementById("total-price").innerText)
+         
+      //      const total=totalPrice+price
+          
+      //       document.getElementById("total-price").innerText=total
+      //        document.getElementById("total-purchase").innerText=total
+      //   })
        
-      }
+      // }
 document.getElementById("coupon-btn").addEventListener("click",function(){
   const totalPrice=Number(document.getElementById("total-price").innerText)
   const couponAmount=Number(document.getElementById("coupon-input").value)
